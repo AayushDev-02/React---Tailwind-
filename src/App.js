8d46a83c9cb99b2acc14096b8b8410525fc8995f
Aayush Yadav <yadavaayush002@gmail.com>
2023-02-09 20:59:20 +0530

    09/02/23

--- src/App.js
+++ src/App.js
@@ -3,22 +3,43 @@ import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import { useState } from 'react';
+import { MoonIcon, SunIcon } from '@heroicons/react/24/solid'
 
 
 function App() {
-  const [darkMode, setdarkMode] = useState(false);
+  const [darkMode, setdarkMode] = useState({
+    color: '#363062',
+    bgColor: '#E9D5CA',
+    icon: MoonIcon,
+  });
+
+  const toggleDarkMode = () => {
+    if (darkMode.bgColor === '#E9D5CA') {
+      setdarkMode({
+        color: '#E9D5CA',
+        bgColor: '#363062',
+        icon: SunIcon,
+      })
+    } else {
+      setdarkMode({
+        color: '#363062',
+        bgColor: '#E9D5CA',
+        icon: MoonIcon,
+      })
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" mode={darkMode} />
+      <Navbar title="TextUtils" color={darkMode.color} Heroicon={darkMode.icon} bg={darkMode.bgColor} enableDarkMode={toggleDarkMode} />
       <div className='grid grid-cols-2 m-10 py-10 '>
         <div className='my-auto w-full font-extrabold ml-10 space-y-10'>
-          <h1 className='text-7xl   text-[#363062]  '>Learning React Js with Aayush Yadav</h1>
+          <div className='text-7xl space-y-5  text-[#363062]  '><div className='text-7xl font-thin'>Learning React Js with</div><div>Aayush Yadav</div></div>
           <button className='w-24 p-3 bg-[#363062]  text-[#E9D5CA] rounded-md font-bold'>Github</button>
         </div>
         <TextForm title="TextUtils" />
       </div>
 
-      <About/>
+      <About />
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-
 export default function Navbar(props) {
   return (
-    <nav className=" border-gray-200 px-2 sm:px-4 py-1  bg-[#363062] sticky top-0">
-        <div className="container flex flex-wrap items-center justify-between mx-auto">
+    <nav className={` border-gray-200 px-2 sm:px-4 py-1 bg-[${props.bg}] `}>
+        <div className={`container flex flex-wrap items-center justify-between mx-auto bg-[${props.bg}]`}>
           <a href="/" className="flex items-center">
-            <span className="self-center text-3xl font-extrabold whitespace-nowrap dark:text-white">{props.title}</span>
+            <span className={`self-center text-3xl font-extrabold whitespace-nowrap text-[${props.color}]`}>{props.title}</span>
           </a>
 
 
             
 
-          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm  rounded-lg md:hidden focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 " aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           </button>
           <div className="hidden w-full  md:w-auto md:flex items-center justify-center" id="navbar-default">
-            <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-[#363062] dark:border-gray-700">
+            <ul className={`flex flex-col p-4 mt-4 border  rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:dark:bg-[${props.bg}] dark:border-gray-700`}>
               <li></li>
               <li>
               <div className='hidden w-1/4 md:flex space-x-5 h-full p-2 '>
-                <span className='font-semibold text-white my-auto'>Search</span>
-                <input placeholder='Search'  className=' bg-[#E9D5CA] rounded-2xl text-black focus:outline-none pl-2 py-1 font-thin text-xs h-7'></input>
+                <span className={`font-semibold text-[${props.color}] my-auto`}>Search</span>
+                <input placeholder='Search'  className={` bg-[${props.color}] rounded-2xl text-black focus:outline-none pl-2 py-1 font-thin text-xs h-7`}></input>
             </div>
               </li>
               <li className='flex items-center justify-center'>
-                <a href="/" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-[#E9D5CA] md:p-0 dark:text-white hover:text-white" aria-current="page">Home</a>
+                <a href="/" className={`block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-[${props.color}] md:p-0 hover:text-white`} aria-current="page">Home</a>
               </li>
               <li className='flex items-center justify-center'>
-                <a href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:text-[#E9D5CA]">{props.aboutText}</a>
+                <a href="/" className={`block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:border-0 md:p-0 md:text-[${props.color}] md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent hover:text-[#E9D5CA]`}>{props.aboutText}</a>
+              </li>
+              <li className={`flex justify-center items-center rounded-full bg-[${props.color}] w-10 h-10 cursor-pointer`} onClick={props.enableDarkMode}>
+              <props.Heroicon className={`h-6 w-6 text-[${props.bg}]`} aria-hidden="true" />
               </li>
               
             </ul>
